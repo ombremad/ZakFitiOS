a14b876f357a7bfc359dd1fed21df40a870ed68b
Anne Ferret <anne@anneferret.eu>
2025-11-24 21:51:30 +0100

    styling

---
+++ ZakFit/Features/Dashboard/Views/DashboardView.swift
@@ -0,0 +1,26 @@
+//
+//  DashboardView.swift
+//  ZakFit
+//
+//  Created by Anne Ferret on 24/11/2025.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    @State var vm = LoginViewModel()
+    var body: some View {
+        VStack {
+            Text("Connecté!")
+            Button("Se déconnecter") {
+                Task {
+                    vm.logout()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    DashboardView()
+}
